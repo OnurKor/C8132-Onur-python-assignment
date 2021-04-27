fd42e69550c207e13df35ed5218e45162a5ce199
OnurKor <81565373+OnurKor@users.noreply.github.com>
2021-04-27 17:44:17 +0300

    Create Assignment-008-2 (Covid-19 Risk).py

---
+++ Assignment-008-2 (Covid-19 Risk).py
@@ -0,0 +1,8 @@
+age = input("Are you a cigarette addict older than 75 years old? (Only Yes or No) ").title().strip()
+chronic = input("Do you have a severe chronic disease? (Only Yes or No) ").title().strip()
+immune = input("Is your immune system too weak? (Only Yes or No) ").title().strip()
+risk = (age == "Yes") or (chronic == "Yes") or (immune == "Yes")
+if risk:
+  print("You are in risky group")
+else :
+  print( "You are not in risky group")
