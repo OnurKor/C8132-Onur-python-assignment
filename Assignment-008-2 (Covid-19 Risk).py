d46a6de6ef454e2a4588ab9495e03710af4b9921
OnurKor <81565373+OnurKor@users.noreply.github.com>
2021-05-29 12:26:22 +0300

    Update Assignment-008-2 (Covid-19 Risk).py

--- Assignment-008-2 (Covid-19 Risk).py
+++ Assignment-008-2 (Covid-19 Risk).py
@@ -2,6 +2,7 @@ age = input("Are you a cigarette addict older than 75 years old? (Only Yes or No
 chronic = input("Do you have a severe chronic disease? (Only Yes or No) ").title().strip()
 immune = input("Is your immune system too weak? (Only Yes or No) ").title().strip()
 risk = (age == "Yes") or (chronic == "Yes") or (immune == "Yes")
+
 if risk:
   print("You are in risky group")
 else :
