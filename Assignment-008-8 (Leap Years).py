c4dfba56b8ef2ceec19d135e865a8e8fc280ad6c
OnurKor <81565373+OnurKor@users.noreply.github.com>
2021-05-31 19:08:51 +0300

    Update Assignment-008-8 (Leap Years).py

--- Assignment-008-8 (Leap Years).py
+++ Assignment-008-8 (Leap Years).py
@@ -1,6 +1,6 @@
 leap = int(input("enter a leap year: "))
 condition = (leap%400 == 0) or (leap % 4 == 0 and leap%100 != 0 )
-if condition:
+if condition :
   print(leap, "is leap year")
 else:
   print(leap, "is not leap year")
